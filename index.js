4d42e5ed8ba9858ce2f10b7f83181433ca7c7f9a
Philip Johnson <pjohnson@cloudflare.com>
2022-01-24 18:03:59 -0600

    added email alerts

--- index.js
+++ index.js
@@ -3,25 +3,27 @@ let responseHeaders = new Headers({
   'Access-Control-Allow-Methods': 'GET, HEAD, POST, OPTIONS',
   'Access-Control-Allow-Headers': '*',
   'content-type': 'application/json;charset=UTF-8',
-});
+})
 
 async function findGuest(submission) {
   const email = submission.email
 
   const params = {
-    filterByFormula: '{Email}="' + email + '"'
+    filterByFormula: '{Email}="' + email + '"',
   }
-  
-  const url = 'https://api.airtable.com/v0/appiPHOcdGktaFlGE/Guests?' + new URLSearchParams(params)
-  
-  let headers = new Headers();
-  headers.append('Authorization', AIRTABLE_API_KEY);
-  headers.append('Content-Type', 'application/json');
-    
+
+  const url =
+    'https://api.airtable.com/v0/appiPHOcdGktaFlGE/Guests?' +
+    new URLSearchParams(params)
+
+  let headers = new Headers()
+  headers.append('Authorization', AIRTABLE_API_KEY)
+  headers.append('Content-Type', 'application/json')
+
   const requestOptions = {
     method: 'GET',
     headers: headers,
-    redirect: 'follow'
+    redirect: 'follow',
   }
   const response = await fetch(url, requestOptions)
   const rj = await response.json()
@@ -30,32 +32,81 @@ async function findGuest(submission) {
   return records
 }
 
+async function sendEmail(submission) {
+  const url = 'https://api.sendgrid.com/v3/mail/send'
+
+  let headers = new Headers()
+  headers.append('Authorization', SENDGRID_API_KEY)
+  headers.append('Content-Type', 'application/json')
+
+  const data = {
+    personalizations: [
+      {
+        to: [
+          {
+            email: 'johnson.philip.louis@gmail.com',
+            name: 'Philip Johnson',
+          },
+        ],
+      },
+    ],
+    from: {
+      email: 'alerts@danielleandphilip.com',
+      name: 'alerts',
+    },
+    replyTo: {
+      email: 'alerts@danielleandphilip.com',
+      name: 'Philip Johnson',
+    },
+
+    subject: submission.email + ' has replied to your save the date',
+    content: [
+      {
+        type: 'text/plain',
+        value: JSON.stringify(submission),
+      },
+    ],
+  }
+
+  const requestOptions = {
+    method: 'POST',
+    headers: headers,
+    body: JSON.stringify(data),
+    redirect: 'follow',
+  }
+
+  const response = await fetch(url, requestOptions)
+  const rj = await response.json()
+
+  return rj
+}
+
 async function createGuest(submission) {
   const email = submission.email
-  
+
   const url = 'https://api.airtable.com/v0/appiPHOcdGktaFlGE/Guests'
-  
-  let headers = new Headers();
-  headers.append('Authorization', AIRTABLE_API_KEY);
-  headers.append('Content-Type', 'application/json');
+
+  let headers = new Headers()
+  headers.append('Authorization', AIRTABLE_API_KEY)
+  headers.append('Content-Type', 'application/json')
 
   const data = {
     records: [
       {
         fields: {
-          'Email': email,
+          Email: email,
           'Guest Names': submission.firstName + ' ' + submission.lastName,
-          'Indicated Liklihood': submission.selectedOption
-        }      
-      }
-    ]
+          'Indicated Liklihood': submission.selectedOption,
+        },
+      },
+    ],
   }
-    
+
   const requestOptions = {
     method: 'POST',
     headers: headers,
     body: JSON.stringify(data),
-    redirect: 'follow'
+    redirect: 'follow',
   }
   const response = await fetch(url, requestOptions)
   const rj = await response.json()
@@ -65,7 +116,9 @@ async function createGuest(submission) {
 }
 
 async function checkExistingResponse(records) {
-  const existingResponses = records.filter(record => typeof record.fields['Indicated Liklihood'] == 'number')
+  const existingResponses = records.filter(
+    record => typeof record.fields['Indicated Liklihood'] == 'number',
+  )
   return existingResponses
 }
 
@@ -76,26 +129,26 @@ async function createAddress(submission) {
         fields: {
           'Address 1': submission.streetAddress,
           'Address 2': submission.streetAddress2,
-          'City': submission.city,
-          'Region': submission.region,
+          City: submission.city,
+          Region: submission.region,
           'Postal Code': submission.postalCode,
-          'Country': submission.country
-        }      
-      }
-    ]
+          Country: submission.country,
+        },
+      },
+    ],
   }
-  
-  const url = 'https://api.airtable.com/v0/appiPHOcdGktaFlGE/Mailing%20Address'  
-  
-  let headers = new Headers();
-  headers.append('Authorization', AIRTABLE_API_KEY);
-  headers.append('Content-Type', 'application/json');
+
+  const url = 'https://api.airtable.com/v0/appiPHOcdGktaFlGE/Mailing%20Address'
+
+  let headers = new Headers()
+  headers.append('Authorization', AIRTABLE_API_KEY)
+  headers.append('Content-Type', 'application/json')
 
   const requestOptions = {
     method: 'POST',
     headers: headers,
     body: JSON.stringify(data),
-    redirect: 'follow'
+    redirect: 'follow',
   }
   const response = await fetch(url, requestOptions)
   const rj = await response.json()
@@ -106,31 +159,30 @@ async function createAddress(submission) {
 
 async function updateRSVP(records, newAddress, submission) {
   const recordsUpdate = records.map(record => {
-    return { 
+    return {
       id: record.id,
       fields: {
         'Indicated Liklihood': submission.selectedOption,
-        'Mailing Address': newAddress
-      }
+        'Mailing Address': newAddress,
+      },
     }
   })
 
   const data = {
-    records: recordsUpdate
+    records: recordsUpdate,
   }
-  
-  const url = 'https://api.airtable.com/v0/appiPHOcdGktaFlGE/Guests'
-    
-  let headers = new Headers();
-  headers.append('Authorization', AIRTABLE_API_KEY);
-  headers.append('Content-Type', 'application/json');
 
-    
+  const url = 'https://api.airtable.com/v0/appiPHOcdGktaFlGE/Guests'
+
+  let headers = new Headers()
+  headers.append('Authorization', AIRTABLE_API_KEY)
+  headers.append('Content-Type', 'application/json')
+
   const requestOptions = {
     method: 'PATCH',
     headers: headers,
     body: JSON.stringify(data),
-    redirect: 'follow'
+    redirect: 'follow',
   }
   const response = await fetch(url, requestOptions)
   const rj = await response.json()
@@ -149,16 +201,20 @@ async function handleRequest(request) {
   const path = requestUrl.pathname
 
   const submission = await request.json()
+
+  const emailResult = await sendEmail(submission)
+  // console.log(JSON.stringify(emailResult))
+
   console.log(JSON.stringify(submission))
-  
+
   // Root Path Logic
-  if(path == '/') {
+  if (path == '/') {
     // console.log(JSON.stringify(request))
-    
+
     // console.log(JSON.stringify(newAddress))
     const guests = await findGuest(submission)
 
-    if(guests.length < 1) {
+    if (guests.length < 1) {
       const newGuest = await createGuest(submission)
       const newAddress = await createAddress(submission)
       console.log(JSON.stringify(newGuest), JSON.stringify(newAddress))
@@ -168,31 +224,30 @@ async function handleRequest(request) {
       const updates = await updateRSVP(guests, newAddress, submission)
     }
 
-   
     return new Response('true', {
       status: 200,
       headers: responseHeaders,
-    });
+    })
   }
 
-  if(path == '/response') {
+  if (path == '/response') {
     const guests = await findGuest(submission)
     const existingResponses = await checkExistingResponse(guests)
     const ej = JSON.stringify(existingResponses)
-    
+
     return new Response(existingResponses.length > 0 ? ej : false, {
       status: 200,
       headers: responseHeaders,
-    });
+    })
   }
 
-  if(path == '/guest') {
+  if (path == '/guest') {
     const guests = await findGuest(submission)
     const ej = JSON.stringify(guests)
-    
+
     return new Response(guests.length > 0 ? ej : false, {
       status: 200,
       headers: responseHeaders,
-    });
+    })
   }
 }
